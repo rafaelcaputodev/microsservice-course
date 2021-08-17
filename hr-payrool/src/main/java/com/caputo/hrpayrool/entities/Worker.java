a912cbc0da1f4b8ccbbfdf2680f15b66e5e98565
Rafael Caputo <rafaelcaputodev@gmail.com>
2021-08-16 23:58:33 -0300

    RestTemplate

---
+++ hr-payrool/src/main/java/com/caputo/hrpayrool/entities/Worker.java
@@ -0,0 +1,18 @@
+package com.caputo.hrpayrool.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Worker implements Serializable {
+    public static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+}
--- hr-payrool/src/main/java/com/caputo/hrpayrool/services/PaymentService.java
+++ hr-payrool/src/main/java/com/caputo/hrpayrool/services/PaymentService.java
@@ -1,13 +1,29 @@
 package com.caputo.hrpayrool.services;
 
 import com.caputo.hrpayrool.entities.Payment;
-import org.springframework.stereotype.Component;
+import com.caputo.hrpayrool.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days){
-        return new Payment("Bob", 200.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", ""+workerId);
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
