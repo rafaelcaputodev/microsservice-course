8657e1e4f9bea5a3cc816e70e5133fa71e4932c0
Rafael Caputo <rafaelcaputodev@gmail.com>
2021-08-30 23:07:25 -0300

    Ribbon load balancing

--- hr-payrool/src/main/java/com/caputo/hrpayrool/services/PaymentService.java
+++ hr-payrool/src/main/java/com/caputo/hrpayrool/services/PaymentService.java
@@ -2,6 +2,7 @@ package com.caputo.hrpayrool.services;
 
 import com.caputo.hrpayrool.entities.Payment;
 import com.caputo.hrpayrool.entities.Worker;
+import com.caputo.hrpayrool.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,17 +14,13 @@ import java.util.Map;
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days){
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", ""+workerId);
 
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
--- hr-worker/src/main/java/com/caputo/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/caputo/hrworker/resources/WorkerResource.java
@@ -2,20 +2,26 @@ package com.caputo.hrworker.resources;
 
 import com.caputo.hrworker.entities.Worker;
 import com.caputo.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
 
     @Autowired
     private WorkerRepository workerRepository;
@@ -27,6 +33,9 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+
+        logger.info("PORT =" + env.getProperty("local.server.port"));
+
         Worker worker = workerRepository.findById(id).get();
         return ResponseEntity.ok(worker);
     }
