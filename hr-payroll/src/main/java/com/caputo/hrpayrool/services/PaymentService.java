02639323daa218ebd96d50ad753ae55e4fc07c2f
Rafael Caputo <rafaelcaputodev@gmail.com>
2022-01-03 22:44:09 -0300

    Project hr-config-server created

--- hr-payroll/src/main/java/com/caputo/hrpayrool/services/PaymentService.java
+++ hr-payroll/src/main/java/com/caputo/hrpayrool/services/PaymentService.java
@@ -18,8 +18,6 @@ public class PaymentService {
     private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days){
-
-
         Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
